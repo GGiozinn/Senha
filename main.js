1fe19eb5059e17b0f5b2f9d9309a858086240eb2
GGiozinn <LANHOSOGIOVANE@GMAIL.COM>
2025-07-31 12:48:00 +0000

    000000

--- main.js
+++ main.js
@@ -9,14 +9,49 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-        tamanhoSenha --;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-}
+            tamanhoSenha --;
+                }
+                    numeroSenha.textContent = tamanhoSenha;
+                    geraSenha();
+                    }
 
-function aumentaTamanho(){
-    if (tamanhoSenha < 20){
-        tamanhoSenha ++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-}
+                    function aumentaTamanho(){
+                        if (tamanhoSenha < 20){
+                                tamanhoSenha ++;
+                                    }
+                                        numeroSenha.textContent = tamanhoSenha;
+                                        geraSenha();
+                                        }
+                                        const campoSenha = document.querySelector('#campo-senha');''
+                                        const checkbox = document.querySelectorAll('.checkbox');
+                                        for (i = 0; i < checkbox.length; i ++){
+                                            checkbox[i].onclick = geraSenha;
+                                        }
+
+                                        const letrasMaiusculas = 'ABCDEFGHJKLMNOPQRSTUVWXYZ';
+                                        const letrasminusculas = 'abcdefghijklmnopqrstuvwxyz';
+                                        const numeros = '123456789';
+                                        const simbolos = '123$%*/?°º';
+                                        geraSenha();
+                                function geraSenha(){
+                                    let alfabeto = '';
+                                    if (checkbox [0].checked){
+                                        alfabeto = alfabeto + letrasMaiusculas;
+                                    }
+                                    if (checkbox [1].checked){
+                                        alfabeto = alfabeto + letrasminusculas;
+                                    }
+                                    if (checkbox [2].checked){
+                                        alfabeto = alfabeto + numeros;
+                                    }
+                                    if (checkbox [3].checked){
+                                        alfabeto = alfabeto + simbolos;
+                                    }
+                                    let senha = '';
+                                    for (let i = 0; i < tamanhoSenha;i++){
+                                        let numeroAleatorio = Math.random()* alfabeto.length;
+                                        numeroAleatorio = Math.floor(numeroAleatorio);
+                                        senha = senha + alfabeto[numeroAleatorio];
+                                    }
+                                    campoSenha.value = senha;
+                                }
